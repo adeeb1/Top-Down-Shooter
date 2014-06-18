90463b3abcbc4ab7d440df7da51e9503819e3dff
tdeeb <tdeeb@ucsc.edu>
2014-06-17 18:50:00 -0700

    Set up MonoGame project
    -Added:
      -Character.cs
      -Enemy.cs
      -LoadAssets.cs
      -MenuScreen.cs

---
+++ Top Down Shooter/Top Down Shooter/LoadAssets.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonoGame.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace Top_Down_Shooter
+{
+    //Load all assets here
+    public static class LoadAssets
+    {
+        //Static variables here
+        static LoadAssets()
+        {
+
+        }
+
+        //Load all content
+        public static void LoadContent(ContentManager Content)
+        {
+            LoadGraphics(Content);
+            LoadMusic(Content);
+            LoadSounds(Content);
+        }
+
+        //Load all graphics
+        public static void LoadGraphics(ContentManager Content)
+        {
+
+        }
+
+        //Load all music
+        public static void LoadMusic(ContentManager Content)
+        {
+
+        }
+
+        //Load all sounds
+        public static void LoadSounds(ContentManager Content)
+        {
+
+        }
+    }
+}
---
+++ Top Down Shooter/Top Down Shooter/Main.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Top_Down_Shooter
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Main : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        //Time the game is active
+        public static float activeTime;
+
+        public Main()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        static Main()
+        {
+            activeTime = 0f;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            //Update active time
+            activeTime += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            
+
+            base.Draw(gameTime);
+        }
+    }
+}
