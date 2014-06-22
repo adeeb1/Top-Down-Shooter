006762a8bb1af4ea42049a6f5916e36451b8ca75
adeeb1 <adeeb1@babson.edu>
2014-06-22 14:50:01 -0400

    Added Windows 8 File Access Helper Methods
    -The methods can be found in Helper.cs

--- Top Down Shooter/Top Down Shooter/Helper.cs
+++ Top Down Shooter/Top Down Shooter/Helper.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Windows.Storage;
 
 namespace Top_Down_Shooter
 {
@@ -34,7 +35,7 @@ namespace Top_Down_Shooter
                     break;
                 case LevelObject.Direction.Up:
                     X = (int)(Parent.ObjectPos.X + (Parent.ObjectTexture.Width / 2) - (Child.ObjectTexture.Width / 2));
-                    Y = (int)(Parent.ObjectPos.Y - Child.ObjectTexture.Height);
+                    Y = (int)(Parent.ObjectPos.Y - Child.null.Height);
 
                     break;
                 case LevelObject.Direction.Down:
@@ -46,6 +47,52 @@ namespace Top_Down_Shooter
 
             // Return a new Vector2 with the coordinates for the child object
             return (new Vector2(X, Y));
+        }
+
+        // Retrieves a subfolder from the app's designated storage location
+        public static async Task<StorageFolder> GetFolder(String FolderName)
+        {
+            // Get the app's local storage folder
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+
+            // Try to get the subfolder in the storage folder. If the folder is not found, the StorageFolder will be null
+            StorageFolder subfolder = (StorageFolder)await folder.TryGetItemAsync(FolderName);
+
+            // Return the subfolder
+            return subfolder;
+        }
+
+        // Creates a new folder in the app's designated storage location
+        public static async void CreateFolder(String FolderName)
+        {
+            // Get the app's local storage folder
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+
+            // Try to create the folder
+            await folder.CreateFolderAsync(FolderName, CreationCollisionOption.OpenIfExists)
+        }
+
+        // Retrieves a file from the app's designated storage location
+        public static async Task<StorageFile> GetFile(String FileName)
+        {
+            // Get the app's local storage folder
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            
+            // Try to get the file from the folder. If the file is not found, the StorageFile will be null
+            StorageFile file = (StorageFile) await folder.TryGetItemAsync(FileName);
+
+            // Return the file
+            return file;
+        }
+
+        // Creates a new file in the app's designated storage location
+        public static async void CreateFile(String FileName)
+        {
+            // Get the app's local storage folder
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+
+            // Try to create the file
+            await folder.CreateFileAsync(FileName, CreationCollisionOption.ReplaceExisting);
         }
 
 
